6ddda846ea418c55055b2e3ac6a937a295f672e5
Manu3lCgBr3ak <manu3lcgbr3ak@gmail.com>
2026-03-28 16:39:18 -0600

    Add files via upload

---
+++ astro.config.mjs
@@ -0,0 +1,22 @@
+// @ts-check
+import { defineConfig } from "astro/config";
+import tailwindcss from "@tailwindcss/vite";
+import node from "@astrojs/node";
+
+export default defineConfig({
+  output: "server",
+
+  adapter: node({
+    mode: "standalone",
+  }),
+
+  image: {
+    service: {
+      entrypoint: "astro/assets/services/sharp",
+    },
+  },
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
+});
